abc4b1da34fbefcdb935345bef733f2895015f46
Anuvind MP <anuvindmp292003@gmail.com>
2024-06-09 18:37:12 +0530

    .

--- ROBOTICS/LAB/LAB3/Lab3Q3.m
+++ ROBOTICS/LAB/LAB3/Lab3Q3.m
@@ -1,14 +1,16 @@
 Rx = rotx(45, 'deg');
 Ry = roty(90, 'deg');
+
 R1 = Ry * Rx;
 R2 = Rx * Ry;
 
 disp('Rotation matrix R1 (Ry * Rx):');
 disp(R1);
+
 disp('Rotation matrix R2 (Rx * Ry):');
 disp(R2);
 
-isEqual = isequal(round(R1, 10), round(R2, 10)); 
+isEqual = isequal(round(R1, 10), round(R2, 10));
 disp('Are the rotation matrices equal?');
 disp(isEqual);
 
@@ -20,4 +22,3 @@ title('Rotation: Ry * Rx');
 subplot(1,2,2);
 trplot(R2, 'frame', 'R2', 'color', 'b');
 title('Rotation: Rx * Ry');
-
